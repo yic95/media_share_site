cc43cf4a3750a2a0dd426b40c79392b6b2291b20
Yichm <0Luke.Luke0@gmail.com>
2025-12-21 16:32:24 +0800

    Fix bugs

--- albumview.php
+++ albumview.php
@@ -4,20 +4,19 @@ include ("util.php");
 
 $passwd = getenv("MEDIA_SHARE_PASSWORD");
 $conn = mysqli_connect("localhost", "media_share", $passwd, "media_share");
+mysqli_set_charset($conn, "utf8mb4");
 $album_id = $_GET["albumid"];
 
-$stat_album_info = $conn->prepare_statement($qry1_album_info);
-if ($has_id) {
-    $stat_album_info->bind_param("i", $album_id);
-    $stat_album_info->execute();
-}
+$stat_album_info = $conn->prepare($qry1_album_info);
+$stat_album_info->bind_param("i", $album_id);
+$stat_album_info->execute();
 $result = $stat_album_info->get_result();
 $album_metadata = mysqli_fetch_array($result);
 $num_row = mysqli_num_rows($result);
 
 $title = "";
 
-if ($num_row == 0 || $album_id == null) {
+if ($num_row == 0 || is_null($album_id)) {
     $title = "找不到合輯！";
 } else {
     $title = $album_metadata[1];
@@ -31,7 +30,7 @@ if ($num_row == 0 || $album_id == null) {
     </head>
     <body>
         <?php
-if ($num_row == 0 || $album_id == null) {
+if ($num_row == 0 || is_null($album_id)) {
     echo <<< DOC
     <h1> 找不到合輯！ </h1>
     <p>
@@ -41,7 +40,7 @@ if ($num_row == 0 || $album_id == null) {
 DOC;
 } else {
     echo "<h1>" . $title . "</h1>";
-    $stat_album_media = $conn->prepare_statement($qry1_album_media);
+    $stat_album_media = $conn->prepare($qry1_album_media);
     $stat_album_media->bind_param("i", $album_id);
     $stat_album_media->execute();
     $media = $stat_album_media->get_result();
@@ -50,7 +49,7 @@ DOC;
     // 以下是媒體的輪盤
     $cnt = 1;
     echo "<div id=\"media_slide_container\"><main>";
-    while ($row = mysqli_fetch_array()) {
+    while ($row = mysqli_fetch_array($media)) {
         // SELECT am.media_id, m.type, ms.location, ms.is_local, mm.description, am.description
         $media_view_url = "mediaview.php?mediaid=" . $row[0];
         $media_location_url = "";
@@ -60,26 +59,26 @@ DOC;
             $media_location_url = "mediaget.php?mediaid=" . $row[0];
         }
 
-        echo "<div id=\"media_slide_" . $cnt . "\" class=\"media_slide\">";
+        echo "<div class=\"media_slide\">";
 
         echo "<h2>" . $cnt . "/" . $media_count . "</h2>";
         
         // TODO 要用CSS把這float到左邊。
-        echo "<div id=\"media_slide_media_\"" . $cnt . "\" class=\"media_slide_media\">";
+        echo "<div class=\"media_slide_media\">";
         if ($row[1] == "image") {
             echo "<a href=\"" . $media_view_url . "\">";
-            echo "<img src=\"" . $media_location_url . "\" alt=\"" . $row[3] . "\">";
+            echo "<img src=\"" . $media_location_url . "\" alt=\"" . htmlentities($row[4]) . "\">";
             echo "</a>";
         } else if ($row[1] == "audio") {
             echo "<audio controls src=\"" . $media_location_url . "\">";
-            echo $row[3];
+            echo strip_tags($row[4]);
             echo "<br />";
             echo "<a href=\"" . $media_location_url . "\">音訊下載連接</a>";
             echo "</audio>";
             echo "<a href=\"" . $media_view_url . "\">詳細資料</a>";
         } else {  // video
             echo "<video controls src=\"" . $media_location_url . "\">";
-            echo $row[3];
+            echo strip_tags($row[4]);
             echo "<br />";
             echo "<a href=\"" . $media_location_url . "\">影片下載連接</a>";
             echo "</video>";
@@ -87,9 +86,11 @@ DOC;
         }
         echo "</div>";  // media_slide_media
 
-        echo text_to_html($row[4]);
+        echo text_to_html(strip_tags($row[5]));
         
         echo "</div>";  // media_slide
+
+        $cnt++;
     }
     echo "</main></div>";  // media_slide_container
 }
